6c2532ebd9297ef5f2904ccdedc8884a00a78688
Andres Campuzano <campuxano88@gmail.com>
2020-04-22 00:22:52 -0500

    Added more original messages.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const messages = [
-	'andres',
-	'ana',
-	'jessica',
-	'diego',
-	'laura',
-	'carolina',
-	'paulina',
+	'It is a pretty good day',
+	'Are you still feeling in pain?',
+	'Better now than tomorrow',
+	'Thanks for code, you are building the future',
+	'You make my day',
+	'I have nothing else to tell you',
+	'Stop talking and lets code',
+	'Are you ready to try #100dayscodechallenge',
+	'#NeverStopLearning',
 ];
 
 const randomMsg = () => {
